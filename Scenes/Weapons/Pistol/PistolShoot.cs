745824ad94fd90c7857912c85097c6e0bd844527
Larry <66905431+larry-oj@users.noreply.github.com>
2024-02-18 01:16:04 +0200

    gun can now shoot

--- Scenes/Weapons/Pistol/PistolShoot.cs
+++ Scenes/Weapons/Pistol/PistolShoot.cs
@@ -1,5 +1,6 @@
 ﻿using Godot;
 using projectthaumaturgy.Scenes.Components.StateMachine;
+using projectthaumaturgy.Scenes.Weapons.CreatedObjects;
 
 namespace projectthaumaturgy.Scenes.Weapons.Pistol;
 
@@ -7,12 +8,25 @@ public partial class PistolShoot : State
 {
     [Export] private PistolIdle _pistolIdle;
     [Export] private Timer _timer;
+    [Export] private Node2D _projectileSpawner;
+    
+    private PackedScene _projectile;
+
+    public override void _Ready()
+    {
+        _projectile = ResourceLoader.Load("res://Scenes/Weapons/CreatedObjects/bullet.tscn") as PackedScene;
+    }
     
     public override void Enter()
     {
         const string animationName = "shooting";
         _animationPlayer.Play(animationName);
         _timer.Start(_animationPlayer.GetAnimation(animationName).Length);
+        
+        var bullet = _projectile.Instantiate() as Bullet;
+        bullet!.Position = new Vector2(_projectileSpawner.GlobalPosition.X, _projectileSpawner.GlobalPosition.Y);
+        bullet!.Rotation = _projectileSpawner.GlobalRotation;
+        GetNode("/root/World").AddChild(bullet);
     }
     
     public override void Exit()
