d2859369c7ca9c764df16c53ba346624e2ab3cd0
Larry <66905431+larry-oj@users.noreply.github.com>
2024-02-25 12:47:57 +0200

    mini health bar component

---
+++ Scenes/Components/UI/MiniHealthBarComponent.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+using projectthaumaturgy.Scenes.Components;
+using projectthaumaturgy.Scripts;
+
+public partial class MiniHealthBarComponent : TextureProgressBar
+{
+	[Export] private HealthComponent _healthComponent;
+	
+	public override void _Ready()
+	{
+		MaxValue = _healthComponent.MaxHealth;
+		Value = _healthComponent.Health;
+		_healthComponent.HealthChanged += OnHealthChanged;
+	}
+
+	private void OnHealthChanged(HealthChange change)
+	{
+		Value = change.AfterHealth;
+	}
+}
