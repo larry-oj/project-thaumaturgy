28e9b1e63d0d68d4209b303712417b50b85333f4
Larry <66905431+larry-oj@users.noreply.github.com>
2024-05-11 13:53:25 +0300

    sprites are now individual resources in game files

--- Resources/Weapons/WeaponResource.cs
+++ Resources/Weapons/WeaponResource.cs
@@ -6,4 +6,5 @@ public partial class WeaponResource : Resource
 	[Export] public PackedScene Scene { get; private set; }
 	[Export] public AtlasTexture IconOutline { get; private set; }
 	[Export] public AtlasTexture IconColor { get; private set; }
+	[Export] public AtlasTexture IconSelect { get; private set; }
 }
