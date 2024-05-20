d5cf209840d52f61dd4ae8fdd6ba9e18cc27a5b3
Larry <66905431+larry-oj@users.noreply.github.com>
2024-05-20 18:57:26 +0300

    settings menu

--- Scenes/UI/ResolutionOptionButton.cs
+++ Scenes/UI/ResolutionOptionButton.cs
@@ -1,14 +1,27 @@
 using Godot;
-using System;
-using projectthaumaturgy.Scenes.Components.StateMachine;
+using Godot.Collections;
+using projectthaumaturgy.Extensions;
 
-public partial class ResolutionOptionButton  : OptionButton
+namespace projectthaumaturgy.Scenes.UI;
+
+public partial class ResolutionOptionButton : OptionButton
 {
+	private Dictionary<long, Vector2I> _res = new()
+	{
+		{0, new(1280, 720)},
+		{1, new(1920, 1080)}
+	};
+	
 	public override void _Ready()
 	{
-		this.AddItem("1280x720", 0);
-		this.AddItem("1920x1080", 1);
+		foreach (var item in _res)
+		{
+			this.AddItem($"{item.Value.X}x{item.Value.Y}", (int)item.Key);
+		}
 		this.Selected = 0;
+		this.ItemSelected += (index) =>
+		{
+			GetWindow().Size = _res[index];
+		};
 	}
 }
-
--- Scenes/UI/WindowOptionButton.cs
+++ Scenes/UI/WindowOptionButton.cs
@@ -1,14 +1,31 @@
 using Godot;
-using System;
-using projectthaumaturgy.Scenes.Components.StateMachine;
+using Godot.Collections;
+using projectthaumaturgy.Extensions;
 
-public partial class WindowOptionButton  : OptionButton
+namespace projectthaumaturgy.Scenes.UI;
+
+public partial class WindowOptionButton : OptionButton
 {
+	private Dictionary<long, DisplayServer.WindowMode> _type = new()
+	{
+		{0, DisplayServer.WindowMode.Windowed},
+		{1, DisplayServer.WindowMode.Fullscreen}
+	};
+	
+	[Export] private OptionButton _resolutionOptionButton;
+	
 	public override void _Ready()
 	{
-		this.AddItem("Windowed", 0);
-		this.AddItem("Fullscreen", 1);
+		foreach (var item in _type)
+		{
+			this.AddItem($"{item.Value.ToString()}", (int)item.Key);
+		}
 		this.Selected = 0;
+		this.ItemSelected += (item) =>
+		{
+			if (item == 1)
+				_resolutionOptionButton.Select(1);
+			DisplayServer.WindowSetMode(_type[item]);
+		};
 	}
-	
 }
