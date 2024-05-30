a1622dda045f20b47f6b0b57883a0532e5fabf43
Larry <66905431+larry-oj@users.noreply.github.com>
2024-05-30 12:29:19 +0300

    change sound slider to control Master bus

--- Scenes/UI/SoundSlider.cs
+++ Scenes/UI/SoundSlider.cs
@@ -11,7 +11,7 @@ public partial class SoundSlider  : HSlider
 	
 	public override void _Ready()
 	{
-		_busIndex = AudioServer.GetBusIndex(Options.Audio.SoundEffectsBus);
+		_busIndex = AudioServer.GetBusIndex(Options.Audio.MasterBus);
 		Value = Mathf.DbToLinear(AudioServer.GetBusVolumeDb(_busIndex));
 		ValueChanged += OnValueChanged;
 		VisibilityChanged += OnVisibilityChanged;
